6aec67d17df49bb58c570e65ba08ef61f451013d
rafaelelizondo-svg <rafaelelizondo@cenyca.edu.mx>
2026-09-09 00:37:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-  const menuBtn = document.getElementById("menuBtn");
-  const navMenu = document.getElementById("navMenu");
-
-  menuBtn.addEventListener("click", function () {
-    navMenu.classList.toggle("active");
-  });
-
-});
+alert("SCRIPT FUNCIONANDO");
