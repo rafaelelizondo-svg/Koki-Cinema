480f44c5c57ec79cdddfc88d07510491862c14ea
rafaelelizondo-svg <rafaelelizondo@cenyca.edu.mx>
2026-09-09 00:39:05 -0700

    Implement menu toggle functionality on click

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("SCRIPT FUNCIONANDO");
+document.addEventListener("DOMContentLoaded", function () {
+
+  const menuBtn = document.getElementById("menuBtn");
+  const navMenu = document.getElementById("navMenu");
+
+  menuBtn.addEventListener("click", function () {
+    navMenu.classList.toggle("active");
+  });
+
+});
